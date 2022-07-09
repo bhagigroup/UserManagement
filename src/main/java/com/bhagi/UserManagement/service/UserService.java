76f9ba40bf36c66694da608a36c1d1b080b905ba
Naresh N <naresh.nadakuditi@gmail.com>
2022-07-09 20:22:40 +0530

    Log messages added.

--- src/main/java/com/bhagi/UserManagement/service/UserService.java
+++ src/main/java/com/bhagi/UserManagement/service/UserService.java
@@ -2,6 +2,8 @@ package com.bhagi.UserManagement.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,12 @@ import com.bhagi.UserManagement.repo.UserRepo;
 @Service
 public class UserService {
 	
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 	@Autowired
 	UserRepo userRepo;
 	
 	public List<Customers> getAllCustomers(){
+		logger.debug("getAllCustomers method called");
 	
 		List<Customers> customerList = userRepo.findAll();
 		
